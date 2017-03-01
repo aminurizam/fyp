62738f23906d11eb91255a7c580115fa8dab4f70
aminurizam <a150547@siswa.ukm.my>
2017-03-02 00:05:25 +0800

    third

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/home', 'HomeController@index');
+    Route::get('/post', 'PostsController@index');
+});
